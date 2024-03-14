fe0879526650e154530e1602892d796d6e5e2c21
Vallyah <boulot@blaise.cardonne.eu>
2024-03-14 18:16:56 +0100

    Move LeafNode class to HTMLNode file

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -9,6 +9,15 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(node.props_to_html(), " href=\"https://www.google.com\" target=\"_blank\"")
         node2 = HTMLNode()
         self.assertEqual(node2.props_to_html(), "")
+
+    def test_leaf(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node2.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+        node3 = LeafNode(None, "This is a raw line")
+        self.assertEqual(node3.to_html(), "This is a raw line")
+        self.assertRaises(ValueError, lambda : LeafNode(None, None))
 
 
 if __name__ == "__main__":
